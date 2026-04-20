9fb35ec4116b8c9e9e001aa54fd15e29769a2fcd
gnoudap <adpham1010@gmail.com>
2026-04-20 11:33:01 +0700

    update: stable package

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    extensions: ['.js', '.jsx', '.json']
-  },
-  server: {
-    port: 3000,
-    open: true
-  },
+  plugins: [react()]
 })
